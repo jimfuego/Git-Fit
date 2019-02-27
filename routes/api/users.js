d6c3f57e3ff5dece8916e0563c5cab5cd580da2e
jimmyB <jimmyb89@yahoo.com>
2019-02-27 08:04:42 -0800

    sorting out dbURI for deploy

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// const keys = require("../../config/keys");
+const keys = require("../../config/keys");
 const MongoClient = require("mongodb").MongoClient;
 
 // Load input validation
@@ -10,7 +10,7 @@ const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 var db;
 
-const dbURI = process.env.MONGODB_URI || require("loginDetails.js");
+const dbURI = process.env.MONGODB_URI || require("./loginDetails.js");
 //const url="http://localhost:27017"
 
 // Use connect method to connect to the server
